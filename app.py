6431e1e8d9c40418e87d7f9238c2eb452904457a
tanwxr <tanwxr@gmail.com>
2026-06-27 21:36:34 +0800

    add datasets

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("PivotCoach")
 st.write("Your Career Transition Navigator")
@@ -8,3 +10,21 @@ page = st.sidebar.selectbox(
     "Navigate",
     ["Home", "Career Gap Explorer", "AI Coach"]
 )
+
+def load_data(nrows):
+    data = pd.read_csv('./data/jobs.csv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! ")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
