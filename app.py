43ccb3e6cf875bd0b2716261b006557a0fe1fc27
tanwxr <tanwxr@gmail.com>
2026-07-19 22:23:04 +0800

    home page

--- app.py
+++ app.py
@@ -38,42 +38,5 @@ View the source code and documentation here:
 
 [GitHub Repository](https://github.com/tanwxr/pivot-coach)
     
-
----
-
-### Preview Raw Data Below
-
 """
 )
-
-
-def load_data(nrows, url):
-    data = pd.read_csv(url, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    return data
-
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading jobs data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000, './data/jobs.csv')
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Jobs Data Loaded! ")
-
-if st.checkbox('Show raw data for jobs'):
-    st.subheader('Raw data extracted from MyCareersFuture')
-    st.write(data)
-
-
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading course data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000, './data/courses.csv')
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Course Data Loaded! ")
-
-if st.checkbox('Show raw data for courses'):
-    st.subheader('Raw data extracted from SkillsFuture')
-    st.write(data)
