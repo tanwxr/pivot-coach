c454389b9536c9e24b0d3048b276925cedc0bece
tanwxr <tanwxr@gmail.com>
2026-07-19 21:43:41 +0800

    debug

--- app.py
+++ app.py
@@ -89,12 +89,15 @@ helping professionals understand **where they are, where they want to go,
 and how to get there.**
 
 
- ### 🔗 Project Repository
+## 🔗 Project Repository
 
 View the source code and documentation here:
 
 [GitHub Repository](https://github.com/tanwxr/pivot-coach)
     
+
+
+### Preview Raw Data Below
 
 """
 )
@@ -108,11 +111,11 @@ def load_data(nrows, url):
 
 
 # Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
+data_load_state = st.text('Loading jobs data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000, './data/jobs.csv')
 # Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! ")
+data_load_state.text("Jobs Data Loaded! ")
 
 if st.checkbox('Show raw data for jobs'):
     st.subheader('Raw data extracted from MyCareersFuture')
@@ -121,11 +124,11 @@ if st.checkbox('Show raw data for jobs'):
 
 
 # Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
+data_load_state = st.text('Loading course data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000, './data/courses.csv')
 # Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! ")
+data_load_state.text("Course Data Loaded! ")
 
 if st.checkbox('Show raw data for courses'):
     st.subheader('Raw data extracted from SkillsFuture')
