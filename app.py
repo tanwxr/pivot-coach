9e66f80bf4b5d544e08a86fe54b94a4b3d817a03
tanwxr <tanwxr@gmail.com>
2026-07-19 22:25:34 +0800

    disclaimer

--- app.py
+++ app.py
@@ -18,6 +18,18 @@ headers = {
     "Authorization": f"Bearer {API_KEY}"
 }
 
+
+st.info(
+"""
+IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+Always consult with qualified professionals for accurate and personalised advice.)
+""")
+
+
+
 st.title("🚀 Pivot Coach")
 
 st.markdown(
